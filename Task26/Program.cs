d4f70007a96e5c3a39fd677660b088841c40fa4d
Tukhel <andrew_melf@mail.ru>
2023-04-19 16:28:37 +1000

    Solution Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int quantlyDigit = QuantlyDigit(number);
+Console.WriteLine($"Количество цифр в числе {number} = {quantlyDigit}");
+
+int QuantlyDigit(int num)
+{
+    int res = 0;
+    while (num != 0)
+    {
+        num /= 10;
+        res++;
+    }
+    return res;
+}
