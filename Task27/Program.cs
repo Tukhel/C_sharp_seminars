854775769d22e2109fe48cab0644ee5181cab7b9
Tukhel <andrew_melf@mail.ru>
2023-04-20 11:02:29 +1000

    Solution Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigit = SumDigit(number);
+Console.WriteLine(sumDigit);
+
+int SumDigit(int num)
+{
+    int result = 0;
+
+    while (num != 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+
+    return result;
+}
