62996c0df8fdf095d96cad423147a11ee001281c
Tukhel <Andrew_melf@mail.ru>
2023-05-22 20:06:33 +1000

    Solution Task56

---
+++ Task56/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] matrix = CreateMatrixRndInt(4, 4, 1, 10);
+PrintMatrix(matrix);
+Console.WriteLine();
+int[] array = SumRowElements(matrix);
+Console.WriteLine();
+int indexMinElement = FindIndexMinElement(array);
+Console.WriteLine($"Номер строки с наименьшей суммой элементов: {indexMinElement + 1}");
+
+
+int FindIndexMinElement(int[] arr)
+{
+    int min = arr[0];
+    int index = 0;
+    for (int i = 1; i < arr.Length - 1; i++)
+    {
+        if (min > arr[i])
+        {
+            min = arr[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+int[] SumRowElements(int[,] matrix)
+{
+    int[] arr = new int[matrix.GetLength(0)];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+
+        arr[i] = sum;
+    }
+
+    return arr;
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine(" |");
+    }
+}
