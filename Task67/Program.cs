51d6d4a8026080de397150879c060f0bcc095487
Tukhel <Andrew_melf@mail.ru>
2023-05-23 22:15:16 +1000

    Solution Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumNumbers(number));
+
+int SumNumbers(int n)
+{
+    if (n == 0) return 0;
+    return n % 10 + SumNumbers(n / 10);
+}
