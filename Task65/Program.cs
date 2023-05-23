380797133f4ef0177e92e53e82a9ce045a5dd8da
Tukhel <Andrew_melf@mail.ru>
2023-05-23 22:15:04 +1000

    Solution Task65

---
+++ Task65/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+
+Console.Write("Введите первое целое положительное число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое положительное число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbersMToN(numberM, numberN);
+
+void NaturalNumbersMToN(int numM, int numN)
+{
+    if (numM < numN)
+    {   
+        Console.Write($"{numM} ");
+        NaturalNumbersMToN(numM + 1, numN);
+        
+    }
+    else if (numM > numN)
+    {
+        Console.Write($"{numM} ");
+        NaturalNumbersMToN(numM - 1, numN);
+    }
+    else Console.Write(numM);
+   }
